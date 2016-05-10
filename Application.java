30508e4225d72ac0b2e4bb6de91272e70c059d48
frankjulio1 <frankjulio1@gmail.com>
2016-05-10 09:56:27 -0300

    push do projeto loja

---
+++ Application.java
@@ -0,0 +1,14 @@
+package loja;
+
+import java.lang.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import groovy.lang.*;
+import groovy.util.*;
+
+public class Application
+  extends grails.boot.config.GrailsAutoConfiguration {
+;
+public static  void main(java.lang.String... args) { }
+}
